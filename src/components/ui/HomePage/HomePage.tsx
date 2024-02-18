294995a3348e1703f4f1bdf5ddbd7c33d88563c1
Agustin Iglesias <agusiglesias72@gmail.com>
2024-02-18 20:35:05 -0300

    git deploy

--- src/components/ui/HomePage/HomePage.tsx
+++ src/components/ui/HomePage/HomePage.tsx
@@ -25,7 +25,7 @@ export default async function HomePage() {
 
   return (
     <div className="flex flex-col items-center justify-center h-screen w-full ">
-      Hola. Soy la página de inicio. Probando Deploys
+      Hola. Soy la página de inicio. Probando Deploysa
       {dollarQuotation.map((dq) => (
         <QuoteCard
           stock_id={dq.stock_id}
